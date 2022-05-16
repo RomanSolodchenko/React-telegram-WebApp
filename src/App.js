d507bdba4c59226fe0bfd3d75175585cd7ce30a8
Roma2021 <IT2021rweb@gmail.com>
2022-05-16 13:13:39 +0300

    interface development 2.2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
 import './App.css';
-
-import Logo from './components/pages/logotip/Logo';
-import NavMenu  from './components/pages/navigation/NavigationMenu';
-import NavigationSocials from './components/pages/navigation_socials/NavigationSocials';
-import Main from './components/pages/main/Main';
-import Footer from './components/pages/footer/Footer';
-
-// import { Menu } from './components/pages/menu/Menu';
-// import RoutesDom from "./components/Routes/RoutesDom";
-
+import Logo from './components/pages/logotip/Logo'
+import Main from './components/pages/main/Main'
+import NavigationMenu from './components/pages/navigation_menu/NavigationMenu';
 function App() {
+
   return (
     <div className="App">
-      <Logo/>
-<NavMenu/>
-<NavigationSocials/>
-<Main/>
-<Footer/>
+<Logo/>
+<NavigationMenu/>
+    <Main/>
+
       </div>
   );
 }
---
+++ src/components/pages/navigation_menu/NavigationMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./NavMenu.css"
+
+
+const NavigationMenu = () => {
+    return (
+        <div className='nav'>
+                    
+                <a className="nav__link" to="#" >Меню</a>
+        
+        </div>
+    );
+}
+
+export default NavigationMenu;
